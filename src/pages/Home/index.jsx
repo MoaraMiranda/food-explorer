a9d25026df4a4764d4d0eae986e291ce8057fa1b
moara <moaramirandass@gmail.com>
2023-12-11 18:49:06 +0100

    Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,17 @@
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 import { Container, Content } from "./styles";
+import { CardList } from "../../components/CardList";
 
 export function Home() {
+  
   return (
     <Container>
       <Header />
-      <Content></Content>
+      <Content>
+        <CardList title= "Meals"/>
+        <CardList title= "Main Dishes"/>
+      </Content>
       <Footer />
     </Container>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,14 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 100vh;
-  display: grid;
-  grid-template-areas:
-    "header"
-    "content"
-    "footer";
   /* flex: 1; */
 `;
 
 export const Content = styled.div`
+  padding: 24px;
   display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
