7481707cf1d993afb8007c53b533cae0bc6adb36
moara <moaramirandass@gmail.com>
2024-01-03 16:32:54 +0100

    card component scroll added

--- src/components/CardList/styles.js
+++ src/components/CardList/styles.js
@@ -4,16 +4,20 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
+  overflow-x: scroll;
 
-  h2 {
+  > h2 {
     color: ${({ theme }) => theme.COLORS.LIGHT_300};
     font-family: "Poppins", sans-serif;
     font-size: 18px;
     font-weight: 500;
     line-height: 140%;
+    padding: 24px 0 0 24px;
   }
-  .list{
+
+  > .list {
     display: flex;
     gap: 16px;
+    padding-left: 24px;
   }
 `;
