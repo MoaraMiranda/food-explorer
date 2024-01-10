ad75366fe79c02b6cc90e98071eb21917795cf77
moara <moaramirandass@gmail.com>
2024-01-10 19:07:03 +0100

    Logotype responsive config

--- src/components/Logotype/styles.js
+++ src/components/Logotype/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
-  width: ${(props) => props.$size || 100}px;
+  width: ${(props) => props.$size || 8.3}rem;
   height: auto;
   > h1 {
     margin-left: 8px;
@@ -14,6 +14,6 @@ export const Container = styled.div`
   }
 
   @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
-    width: ${(props) => props.$desktopSize || 100}px;
+    width: ${(props) => props.$desktopSize || 8.3}rem;
   }
 `;
