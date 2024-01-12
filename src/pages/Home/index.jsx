20f95b9f3685fb36082debdb0a7babc7ee80fe00
moara <moaramirandass@gmail.com>
2024-01-12 19:18:44 +0100

    Home banner added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,21 @@
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import { Container, Content } from "./styles";
+import { Container, Content, BannerSection } from "./styles";
 import { CardList } from "../../components/CardList";
+import bannerImg from "../../assets/macaron.png";
 
 export function Home() {
   return (
     <Container>
       <Header />
-      <Content>
+      <Content className="containerWrapper">
+        <BannerSection>
+          <img src={bannerImg} alt="Macarons image" />
+          <div>
+          <h1>Unbelievable tastes</h1>
+          <p>Feel the care of preparation with selected ingredients.</p>
+          </div>
+        </BannerSection>
         <CardList title="Meals" />
         <CardList title="Main Dishes" />
         <CardList title="Favorites" />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
   height: 100vh;
@@ -8,4 +9,35 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   gap: 24px;
+  margin: 0 auto;
+`;
+
+export const BannerSection = styled.div`
+  display: none;
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
+    display: flex;
+    height: 260px;
+    margin-top: 164px;
+    background: linear-gradient(180deg, #091e26 0%, #00131c 100%);
+    position: relative;
+    justify-content: center;
+    width: 100%;
+
+    > img {
+      width: 632px;
+      height: 406px;
+      position: absolute;
+      bottom: 0;
+      left: -40px;
+    }
+    > div {
+      display: flex;
+      flex-direction: column;
+      align-items: left;
+      position: absolute;
+      right: 100px;
+      top: 100px;
+    }
+  }
 `;
