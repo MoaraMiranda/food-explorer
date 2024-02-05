1c2c6eeeedf85cc4a23b76fb12528dbc8fd1a3ac
moara <moaramirandass@gmail.com>
2024-02-05 17:49:27 +0100

    EditDish page added

---
+++ src/pages/admin/EditDish/index.jsx
@@ -0,0 +1,54 @@
+import { BackLink } from "../../../components/BackLink";
+import { Button } from "../../../components/Button";
+import { Footer } from "../../../components/Footer";
+import { Header } from "../../../components/Header";
+import { DownloadImageButton } from "../../../components/DownloadImageButton";
+import { Input } from "../../../components/Input";
+import { Textarea } from "../../../components/Textarea";
+import { TagsController } from "../../../components/Tagscontroller";
+import { Container, Content, Form } from "./styles";
+import { DropdownList } from "../../../components/DropdownList";
+
+export function EditDish() {
+  return (
+    <Container>
+      <Header isAdmin />
+      <Content className="containerWrapper">
+        <BackLink title="Back" route="/admin" />
+        <Form className="form-container">
+          <fieldset>
+            <legend>Edit Dish</legend>
+            <DownloadImageButton id="image" title="Select Image" />
+            <Input
+              id="name"
+              placeholder="Ex.: Ceasar Salad"
+              type="text"
+              title="Name"
+              label
+            />
+            <DropdownList />
+            <section className="add-ingredients-section">
+              <h2>Ingredients</h2>
+              <div className="tags">
+                <TagsController $isNew placeholder="Add" />
+                <TagsController placeholder="Lettuce" />
+                <TagsController placeholder="White Bread" />
+                <TagsController placeholder="Tomato" />
+              </div>
+            </section>
+            <Input
+              id="price"
+              placeholder="€ 10,50"
+              type="text"
+              title="Price"
+              label
+            />
+            <Textarea />
+            <Button title="Save" />
+          </fieldset>
+        </Form>
+      </Content>
+      <Footer />
+    </Container>
+  );
+}
---
+++ src/pages/admin/EditDish/styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../../styles/deviceBreakpoints";
+
+export const Container = styled.div`
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const Form = styled.form`
+  width: 100%;
+
+  > fieldset {
+    display: grid;
+    gap: 24px;
+    border: none;
+
+    > legend {
+      font-family: "Poppins", sans-serif;
+      font-size: 32px;
+      font-weight: 500;
+      margin-bottom: 32px;
+    }
+
+    .tags {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: start;
+      gap: 16px;
+      background-color: ${({ theme }) => theme.COLORS.DARK_800};
+      padding: 8px;
+      border-radius: ${({ theme }) => theme.RADIUS.MD};
+    }
+
+    .add-ingredients-section {
+      display: grid;
+      gap: 16px;
+
+      > h2 {
+        font-size: 1.3rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT_400};
+        font-weight: 400;
+      }
+    }
+  }
+`;
