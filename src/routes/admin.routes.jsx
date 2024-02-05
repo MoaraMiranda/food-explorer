b3a2c0f809c02e83487920e4b59ef1aa2e602b5b
moara <moaramirandass@gmail.com>
2024-02-05 17:48:37 +0100

    Edit dish route added

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { DishDetails } from "../pages/admin/DishDetails";
 import { Home } from "../pages/admin/Home";
 import { NewDish } from "../pages/admin/NewDish";
+import { EditDish } from "../pages/admin/EditDish";
 
 export function AdminRoutes() {
   return (
@@ -10,6 +11,7 @@ export function AdminRoutes() {
       <Route path="/admin" element={<Home />} />
       <Route path="/admin/dish-details/:id" element={<DishDetails />} />
       <Route path="/admin/new-dish" element={<NewDish />} />
+      <Route path="/admin/edit-dish" element={<EditDish />} />
     </Routes>
   );
 }
